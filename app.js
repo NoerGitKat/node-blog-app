6852b087c64e711d187a9b6760ba9b1f80c1f868
Noer <noerpaanakker@hotmail.com>
2017-06-01 16:58:34 +0200

    added jquery, created 3 pug files for index, login, register

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //require express, pg, sequelize, bodyparser
-const app = require('express')();
+const express = require('express');
+const app = express();
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/nodeblog');
 const bodyParser = require('body-parser')();
 
 app.use('/', bodyParser);
+app.use(express.static('public'));
 
 app.set('views', 'views');
 app.set('view engine', 'pug');
@@ -26,4 +28,40 @@ const Comment = db.define('comment', {
 	body: Sequelize.STRING
 })
 
-//
+//Creating all routes first
+app.get('/', (req, res) => {
+	//homepage
+	res.render('index');
+});
+
+app.get('/register', (req, res) => {
+	//register account route
+});
+
+app.post('/register', (req, res) => {
+	//register account route
+});
+
+app.get('/login', (req, res) => {
+	//login account get route
+});
+
+app.post('/login', (req, res) => {
+	//login area
+});
+
+app.get('/', (req, res) => {
+	//redirect to homepage when logout
+});
+
+app.get('/', (req, res) => {
+	//view all own posts
+});
+
+app.get('/', (req, res) => {
+	//view specific post + ajax
+});
+
+const server = app.listen(3000, () => {
+	console.log("The server has started at port:" + server.address().port);
+});
