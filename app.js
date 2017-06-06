62bfff1a247ebcca10f363c54364232ec6841501
Noer <noerpaanakker@hotmail.com>
2017-06-06 18:03:51 +0200

    added form in register pug file, deleted some useless routes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/nodeblog');
 const bodyParser = require('body-parser')();
 const session = require('express-sessions');
-const tables = requirel('./tables.js')
 
 app.use('/', bodyParser);
 app.use(express.static('public'));
@@ -13,42 +12,57 @@ app.use(express.static('public'));
 app.set('views', 'views');
 app.set('view engine', 'pug');
 
+//Creating tables necessary for login + posts/comments
+const User = db.define('user', {
+	username: Sequelize.STRING,
+	firstname: Sequelize.STRING,
+	lastname: Sequelize.STRING,
+	email: Sequelize.STRING,
+	password: Sequelize.STRING
+});
+
+const Post = db.define('post', {
+	title: Sequelize.STRING,
+	body: Sequelize.STRING
+})
+
+const Comment = db.define('comment', {
+	body: Sequelize.STRING
+})
 
 //Creating all routes first
 app.get('/', (req, res) => {
-	//homepage
-	res.render('index');
+	res.render('index');			//homepage
 });
 
 app.get('/register', (req, res) => {
-	//register account route
-	res.render('register');
+	res.render('register');			//register page
 });
 
 app.post('/register', (req, res) => {
-	//register account route
+	//connect to database
+	//insert new data in db
+	//redirect to members area w/ posts
 
 });
 
 app.get('/login', (req, res) => {
-	//login account get route
+	//render login file
 });
 
 app.post('/login', (req, res) => {
-	//login area
+	//connect to database
+	//find match data
+
 });
 
-app.get('/', (req, res) => {
-	//redirect to homepage when logout
+app.get('/posts', (req, res) => {
+	//render posts file with variable
+	//connect to database
+	//show all posts
+	//create option to post new post in posts pug file
 });
 
-app.get('/', (req, res) => {
-	//view all own posts
-});
-
-app.get('/', (req, res) => {
-	//view specific post + ajax
-});
 
 const server = app.listen(3000, () => {
 	console.log("The server has started at port:" + server.address().port);
